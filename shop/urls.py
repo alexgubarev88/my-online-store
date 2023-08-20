88947c4667ab8149890660a56a6d755576442d8f
alexg <alexgubarev885@gmail.com>
2023-08-20 13:53:03 +0300

    Make shop views as class view

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import ProductListView, ProductDetailView
 
 app_name = "shop"
 urlpatterns = [
-    path("", views.product_list, name="product_list"),
-    path("<slug:category_slug>/", views.product_list, name="product_list_by_category"),
-    path("<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("", ProductListView.as_view(), name="product_list"),
+    path("<slug:category_slug>/", ProductListView.as_view(), name="product_list_by_category"),
+    path("<int:id>/<slug:slug>/", ProductDetailView.as_view(), name="product_detail"),
 ]
+
