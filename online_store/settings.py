2cf8d273095f94ba84e820637d1239374382be0c
alexg <alexgubarev885@gmail.com>
2023-08-26 10:41:29 +0300

    Staticfiles managing

--- online_store/settings.py
+++ online_store/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 CART_SESSION_ID = "cart"
