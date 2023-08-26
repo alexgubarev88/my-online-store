87b3413ac7d47e8a75a2d956bf5bb0120c37a3ef
alexg <alexgubarev885@gmail.com>
2023-08-26 11:01:17 +0300

    Removed insecure Django secret key from settings

--- online_store/settings.py
+++ online_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-h)$$=nxehcd888uqk00t720hmlj-n1kmvi2nxj2a#@^ywh3^+n")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
